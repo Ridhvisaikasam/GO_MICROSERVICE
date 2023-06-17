d6c515fc046b07057752ef4475262778c8ffabca
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2023-06-17 21:59:19 +0530

    helloworld go server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// main entry point
+func main() {
+
+	//path(when matched or not matches any other),function to be executed which handles http req and res
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("hello")
+		d, err := ioutil.ReadAll(r.Body)
+		//error handling
+		if err != nil {
+			http.Error(rw, "oops", http.StatusBadRequest)
+			//rw.WriteHeader(http.StatusBadRequest)
+			//rw.Write([]byte("oops"))
+			return
+		}
+
+		fmt.Fprintf(rw, "Data %s \n", d)
+	})
+
+	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
+		log.Println("bye")
+	})
+
+	//listen at port,serve handler(interface implementing servehttp)(nil->default servemux(redirecting paths))
+	http.ListenAndServe(":9090", nil)
+
+}
+
+//good structured packages required for microservices
