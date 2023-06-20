04f74bd3a05a777b877dbec3e1e19485220771f8
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2023-06-20 10:36:14 +0530

    created a handlers package ,,, but not able to import itgit status

---
+++ handlers/hello.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+//handler is an interface with just serveHTTP method with rw and req
+
+// something related to dependancy injection
+type Hello struct {
+	l *log.Logger
+}
+
+// in test we can just replace logger to anything (configuring in broader level)
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+// add method to satisfy interface
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	//dont create concrete objects inside handlers like loggers or db
+	h.l.Println("hello")
+	d, err := ioutil.ReadAll(r.Body)
+	//error handling
+	if err != nil {
+		http.Error(rw, "oops", http.StatusBadRequest)
+		//rw.WriteHeader(http.StatusBadRequest)
+		//rw.Write([]byte("oops"))
+		return
+	}
+
+	fmt.Fprintf(rw, "Data %s \n", d)
+}
--- main.go
+++ main.go
@@ -1,36 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	//need to import handlers package
 	"log"
 	"net/http"
+	"os"
 )
 
 // main entry point
 func main() {
 
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	//all the content in handle func into an independent object
+	hh := handlers.NewHello(l)
+
+	//new servemux
+	sm := http.NewServeMux()
+	//register handler to servemux
+	sm.Handle("/", hh)
+
+	//register handler with the server
+	//converting function to handle and registering to defaultservemux(server multiplexer for multiple paths)
+	//http.HandleFunc()
+
 	//path(when matched or not matches any other),function to be executed which handles http req and res
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("hello")
-		d, err := ioutil.ReadAll(r.Body)
-		//error handling
-		if err != nil {
-			http.Error(rw, "oops", http.StatusBadRequest)
-			//rw.WriteHeader(http.StatusBadRequest)
-			//rw.Write([]byte("oops"))
-			return
-		}
-
-		fmt.Fprintf(rw, "Data %s \n", d)
+	/*http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {......
 	})
-
 	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
 		log.Println("bye")
-	})
+	})*/
 
 	//listen at port,serve handler(interface implementing servehttp)(nil->default servemux(redirecting paths))
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 
 }
 
