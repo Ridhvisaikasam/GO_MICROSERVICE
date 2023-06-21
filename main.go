ca990a833a3391b89c4c81c4fc42b02020d9b3b9
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2023-06-21 19:13:32 +0530

    added graceful shutdown

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"go_microservice/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 // main entry point
 func main() {
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api(logger)", log.LstdFlags)
 	//all the content in handle func into an independent object
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
@@ -20,6 +23,19 @@ func main() {
 	//register handler to servemux
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
+
+	/*timeouts are imp -- resources are finite , if client pauses (blocked conn) ,,,
+	multiple blocked connections -- server fails
+	service denial attack*/
+
+	//tuning elements by manually creating a server(http)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
 	//register handler with the server
 	//converting function to handle and registering to defaultservemux(server multiplexer for multiple paths)
@@ -33,7 +49,36 @@ func main() {
 	})*/
 
 	//listen at port,serve handler(interface implementing servehttp)(nil->default servemux(redirecting paths))
-	http.ListenAndServe(":9090", sm)
+	//http.ListenAndServe(":9090", sm)
+
+	//refactor a little because of shutdown
+	//handling in go func so its not gonna block
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			//print followed by os.exit()
+			l.Fatal(err)
+		}
+	}()
+
+	//broadcast a message on a channel whenever os is interrupted or killed
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	//block here until a message available to conceive ,,, once conceived shutdown
+	sig := <-sigChan
+	l.Println("Received terminate ,graceful shutdown :: ", sig)
+
+	/*graceful shutdown -imp- during large file upload/db transaction of client
+	now we need to upgrade the server ,,, if we shutdown we are obstructing clients task
+	but with GO server after shutdown() called
+	doesnot allow new req ,, wait till all the requests completed ,, program will exit
+	*/
+
+	//this implies that allow 30 sec to do all of work and then forcefully close them
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 
 }
 
